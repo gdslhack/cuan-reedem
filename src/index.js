f22bae5fa32343690248f7c54488661556cf1e6e
Dedy Setiadi Pramadan <34924797+gdslhack@users.noreply.github.com>
2024-09-05 05:45:46 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const API_SEMPAG = process.env.API_SEMPAG; // URL API Telkomsel yang disembunyikan
+const API_SEMPAG = process.env.API_SEMPAG; 
 
 app.use(cors());
 app.use(bodyParser.json());
